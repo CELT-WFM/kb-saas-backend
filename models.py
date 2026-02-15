715cb3bd32c48d8f13cb648487f008843b4572a2
CELT-WFM <david@one-2-all.eu>
2026-02-15 20:50:57 +0000

    Update models.py
    Add code

--- models.py
+++ models.py
@@ -1,1 +1,26 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.sql import func
+from db import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+
+class Collection(Base):
+    __tablename__ = "collections"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+class Document(Base):
+    __tablename__ = "documents"
+    id = Column(Integer, primary_key=True, index=True)
+    filename = Column(String)
+    s3_key = Column(String)
+    uploaded_at = Column(DateTime(timezone=True), server_default=func.now())
+    owner_id = Column(Integer, ForeignKey("users.id"))
+    collection_id = Column(Integer, ForeignKey("collections.id"))
+    file_type = Column(String)
 
