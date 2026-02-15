d3aa2eeea8213a98269db6a0dc373696ea045dbd
CELT-WFM <david@one-2-all.eu>
2026-02-15 20:50:28 +0000

    Update db.py
    Add Code

--- db.py
+++ db.py
@@ -1,1 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL")  # Railway Postgres connection string
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
